d000c3e0e681c9d0e797b1d4bfc157e5260975f6
Sofia Araujo <sofiamarcelino2804@gmail.com>
2024-02-03 12:30:50 -0300

    change game mechanics for up to 5 points

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function getComputerChoice(){
     }
 }
 
-
 function playerRound(computerChoice, playerChoice){
     computerChoice = computerChoice.toUpperCase()
     playerChoice = playerChoice.toUpperCase()
@@ -26,31 +25,41 @@ function playerRound(computerChoice, playerChoice){
     }
 }
 
-function playGame(playerChoice){
+function playGame(){
     let computerScore = 0
     let playerScore = 0
-    for(i = 0; i < 5; i++){
-        let playerChoice = prompt("Write your choice!")
-        let computerChoice = getComputerChoice()
-        if(playerRound(computerChoice, playerChoice) == "You Win!"){
-            playerScore++
-        }else if(playerRound(computerChoice, playerChoice) == "You Lose!"){
-            computerScore++
-        }else {
-            computerScore++
-            playerScore++
-        }
-        console.log("Player Choice: " + playerChoice + "\n" + "Computer Choice: " + computerChoice)
-        console.log("Round: " + (i + 1) + "\n" + "---- The score -----\nPlayer: " + playerScore + "\n"+ "Computer: " + computerScore)
-    }
-    if(computerScore > playerScore){
-        return "You lose!"
-    }else if(playerScore > computerScore){
-        return "You Win!"
-    }else {
-        return "Draw in the game"
-    }
-}
-
-
-console.log(playGame())
+    let playerChoice = ""
+    let rock = document.querySelector("#rock")
+    let paper = document.querySelector("#paper")
+    let scissors = document.querySelector("#scissors")
+    let buttons = document.querySelectorAll("button")
+        buttons.forEach(button => {
+            button.addEventListener("click", function(){
+                playerChoice = this.textContent
+                let computerChoice = getComputerChoice()
+                if(playerRound(computerChoice, playerChoice) == "You Win!"){
+                    playerScore++
+                }else if(playerRound(computerChoice, playerChoice) == "You Lose!"){
+                    computerScore++
+                }else {
+                    computerScore++
+                    playerScore++
+                }
+                console.log("Player Choice: " + playerChoice + "\n" + "Computer Choice: " + computerChoice)
+                console.log("Round: " + "\n" + "---- The score -----\nPlayer: " + playerScore + "\n"+ "Computer: " + computerScore) 
+                if(computerScore == 5 || playerScore == 5){
+                    if(computerScore > playerScore){
+                        console.log("You Lose!")
+                        return
+                        }else if(playerScore > computerScore){
+                        console.log("You win!")
+                        return
+                        }else {
+                        console.log("Draw in the game!")
+                        return
+                        }
+                }
+            })})    
+    } 
+    
+playGame()
